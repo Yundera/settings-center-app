20c42edf66aa4387f0986bec66034a2c1495a873
PH <pierre.henri@aptero.co>
2025-01-03 15:55:14 +0100

    format

--- settings-dashboard/src/app/pages/App.tsx
+++ settings-dashboard/src/app/pages/App.tsx
@@ -16,10 +16,14 @@ import {softDarkTheme, softLightTheme} from "@/app/pages/softTheme";
 import {Dashboard} from "@/app/pages/Dashboard";
 import {AppWrapper} from "@/app/pages/AppWrapper";
 
-const MyApp = ({authProvider,dataProvider,permissions}:{authProvider:EmailAuthProviderInterface,dataProvider:any,permissions:Record<string,boolean>}) => {
+const MyApp = ({authProvider, dataProvider, permissions}: {
+  authProvider: EmailAuthProviderInterface,
+  dataProvider: any,
+  permissions: Record<string, boolean>
+}) => {
 
-  let availablePanels:any[] = []
-  let panels:any[] = [];
+  let availablePanels: any[] = []
+  let panels: any[] = [];
   for (const availablePanel of availablePanels) {
     if (availablePanel.permissions && !!permissions[availablePanel.permissions]) {
       panels.push(availablePanel);
@@ -33,12 +37,12 @@ const MyApp = ({authProvider,dataProvider,permissions}:{authProvider:EmailAuthPr
       authProvider={authProvider}
       dataProvider={dataProvider}
       themeList={[
-        { name: 'default', light: defaultLightTheme, dark: nanoLightTheme },
-        { name: 'soft', light: softLightTheme, dark: softDarkTheme },
-        { name: 'classic', light: defaultLightTheme, dark: defaultDarkTheme },
-        { name: 'nano', light: nanoLightTheme, dark: nanoDarkTheme },
-        { name: 'radiant', light: radiantLightTheme, dark: radiantDarkTheme },
-        { name: 'house', light: houseLightTheme, dark: houseDarkTheme },
+        {name: 'default', light: defaultLightTheme, dark: nanoLightTheme},
+        {name: 'soft', light: softLightTheme, dark: softDarkTheme},
+        {name: 'classic', light: defaultLightTheme, dark: defaultDarkTheme},
+        {name: 'nano', light: nanoLightTheme, dark: nanoDarkTheme},
+        {name: 'radiant', light: radiantLightTheme, dark: radiantDarkTheme},
+        {name: 'house', light: houseLightTheme, dark: houseDarkTheme},
       ]}
       dashboard={Dashboard}
       panels={panels}
